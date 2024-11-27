5f9bffb119b39ffa156b864cf15501559a782b49
Dimitrij Brechko <brechkodm@gmail.com>
2024-11-27 19:35:35 +0300

    Commit

---
+++ app/src/main/java/com/example/checkboxsnackbar2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.checkboxsnackbar2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.checkboxsnackbar2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.apply {
+            checkCB.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    rulesTV.text = getString(R.string.pdd)
+                } else {
+                    rulesTV.text = getString(R.string.info)
+                }
+            }
+        }
+
+    }
+}
